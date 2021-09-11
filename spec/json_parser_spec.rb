da8e90ad39e023e69efba8100c23c2d9b3c6923b
AngelMoraDev <angelmomaa@gmail.com>
2021-09-11 18:31:09 -0500

    add transformer specs

---
+++ spec/json_parser_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require_relative '../bin/main'
+
+describe JsonParser do
+  describe '#initialize' do
+    it "doesn't reads provided URI unless header setup" do
+      stub_request(:get, 'https://test-users-2020.herokuapp.com/api/users').to_return(status: 401)
+      new_request = JsonParser.new('https://test-users-2020.herokuapp.com/api/users', '')
+      expect(new_request.parsed_json.code).to eq(401)
+    end
+
+    it 'reads provided URI with auth header' do
+      stub_request(:get, 'https://test-users-2020.herokuapp.com/api/users')
+        .with(
+          headers: {
+            Authorization: 'abc123'
+          }
+        )
+        .to_return(status: 200)
+      authenticated = JsonParser.new('https://test-users-2020.herokuapp.com/api/users', 'abc123')
+      expect(authenticated.parsed_json.code).to eq(200)
+    end
+  end
+end
---
+++ spec/json_transformer_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require_relative '../bin/main'
+require 'byebug'
+
+describe JsonTransformer do
+  describe '#initialize' do
+    it 'gets the json body' do
+      file = File.read('./spec/transformable.json')
+      json_data = JSON.parse(file)
+      requested = JsonTransformer.new(json_data)
+      expect(requested.json_body).to be_a_kind_of(Array)
+    end
+  end
+
+  describe '#valid?' do
+    it 'validates firstName'
+    it 'validates lastName'
+    it 'validates email or phone present' do
+      it 'considers both parenthesized and plain number'
+    end
+  end
+
+  describe '#remove_duplicates' do
+    it 'merges users with same first and last names'
+  end
+
+  describe '#set_properly' do
+    it 'takes phone data one level up'
+  end
+
+  describe '#transform' do
+    it 'executes json cleaning properly'
+  end
+
+  describe '#export' do
+    it "can't export if no valid users"
+    it 'exports properly'
+  end
+end
