81ff93ffdf56c96c95fb5524cd26d1d92d163ce3
AngelMoraDev <angelmomaa@gmail.com>
2021-09-10 20:12:27 -0500

    add transformer to lib, set call with link and header arguments in the new instance call

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,4 @@
 require_relative '../lib/json_parser'
+require_relative '../lib/request_transformer'
 
-JsonParser.new
+JsonParser.new('https://test-users-2020.herokuapp.com/api/users', 'abc123')
---
+++ lib/request_transformer.rb
@@ -0,0 +1,13 @@
+class RequestTransformer
+  def transform
+    # do the data wraggling
+  end
+
+  def export
+    # export new json locally
+  end
+
+  def parsable?
+    # check if json file is a valid json in ruby
+  end
+end
