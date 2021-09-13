ae77ddee4dff4ffeeca2584fd38e851ca5e937a9
AngelMoraDev <angelmomaa@gmail.com>
2021-09-13 00:05:57 -0500

    add transform method

--- bin/main.rb
+++ bin/main.rb
@@ -2,4 +2,4 @@ require_relative '../lib/json_parser'
 require_relative '../lib/json_transformer'
 
 request = JsonParser.new('https://test-users-2020.herokuapp.com/api/users', 'abc123').parsed_json.body
-JsonTransformer.new(request)
+JsonTransformer.new(request).transform
